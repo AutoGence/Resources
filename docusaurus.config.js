8b6f1981ce69719399f367ff27e5b0f0e9355dce
Kang, Cao <techmaster@autogence.ai>
2025-11-07 10:10:53 +0000

    fixed a bug in restored the default layout

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,7 +99,7 @@ const config = {
       },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
-      // Hide the table of contents on the right
+      // Table of contents configuration (shown on right sidebar)
       tableOfContents: {
         minHeadingLevel: 2,
         maxHeadingLevel: 4,
