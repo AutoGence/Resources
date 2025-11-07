c5ed4fbae53b45d257a4d2718487c8ec58153ecb
Kang, Cao <techmaster@autogence.ai>
2025-11-07 09:52:25 +0000

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
+
 function ResourcesHeader() {
   return (
     <header className={clsx('hero', styles.heroBanner)}>
